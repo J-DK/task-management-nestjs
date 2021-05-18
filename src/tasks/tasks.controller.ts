ce102e5dfcd4dbe00767836340a1c1c3398566ae
J-DK <dileepkumarjami@gmail.com>
2021-05-18 22:43:31 +0530

    feat: updated the status of task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
@@ -20,6 +20,11 @@ export class TasksController {
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status')
+  updateStatusOfTaskById(@Param('id') id: string, @Body('status') status: TaskStatus): Task {
+    return this.tasksService.updateStatusOfTaskById(id, status);
   }
 
   @Delete('/:id')
