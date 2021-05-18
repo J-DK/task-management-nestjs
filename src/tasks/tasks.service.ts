fe0c88401530ca2aa388a50570affa4773f91706
J-DK <dileepkumarjami@gmail.com>
2021-05-18 23:22:56 +0530

    refactor: added validation for non existing task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -25,7 +25,6 @@ export class TasksService {
           task.description.includes(search) || task.title.includes(search),
       );
     }
-    console.log('tasks filtered', tasks);
     return tasks;
   }
 
@@ -43,7 +42,12 @@ export class TasksService {
   }
 
   getTaskById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const found = this.tasks.find((task) => task.id === id);
+
+    if (!found) {
+      throw new NotFoundException('Task with id ' + id + ' is not found');
+    }
+    return found;
   }
 
   deleteTaskById(id: string): void {
