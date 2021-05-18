2509854740a19aa3b098371c2a9dda17bb43ccc6
J-DK <dileepkumarjami@gmail.com>
2021-05-18 22:11:15 +0530

    feat(dto): created a new dto for creating tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from "./dto/create-task.dto";
 
 @Injectable()
 export class TasksService {
@@ -10,11 +11,12 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, desc: string): Task {
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title,
-      description: desc,
+      description,
       status: TaskStatus.OPEN,
     };
 
