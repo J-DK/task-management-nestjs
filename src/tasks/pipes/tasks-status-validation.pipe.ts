e69c805844db7c1020da788a5c9f42f5b8a3bf36
J-DK <dileepkumarjami@gmail.com>
2021-05-18 23:35:46 +0530

    feat: added functionality to status validation pipe

--- src/tasks/pipes/tasks-status-validation.pipe.ts
+++ src/tasks/pipes/tasks-status-validation.pipe.ts
@@ -1,9 +1,22 @@
-import { ArgumentMetadata, PipeTransform } from '@nestjs/common';
+import { BadRequestException, PipeTransform } from "@nestjs/common";
+import { TaskStatus } from '../task.model';
 
-export class TasksStatusValidationPipe implements PipeTransform{
+export class TasksStatusValidationPipe implements PipeTransform {
 
+  readonly allowedStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
   transform(value: any): any {
-    console.log(value);
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} is an invalid status`);
+    }
     return value;
+  }
+
+  private isStatusValid(status: any): boolean {
+    return this.allowedStatuses.indexOf(status) !== -1;
   }
 }
