ee1d8fa791144b65992e328218eda17f7bde12aa
J-DK <dileepkumarjami@gmail.com>
2021-05-20 21:20:59 +0530

    feat(signup): added validation rules for username and password

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least one lower case letter, one uppercase latter and a number or special character',
+  })
   password: string;
 }
